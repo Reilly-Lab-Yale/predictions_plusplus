de0d84c3ad3779b7fe5ff0ed31d74a826626ac23
Mackenzie Noon <mackenzie.noon@yale.edu>
2025-07-17 15:56:05 -0400

    +5000 pilot

---
+++ big_runs/pilot5000/pilot_array_5000.sh
@@ -0,0 +1,58 @@
+#!/bin/bash
+#SBATCH --job-name=mpac_pilot
+#SBATCH --partition=gpu
+#SBATCH --gpus 1
+#SBATCH -c 1
+#SBATCH --mem-per-cpu=64G
+#SBATCH -t 1-00:00:00
+#SBATCH --array=0-230
+
+module load miniconda
+conda activate boda_cu128
+
+chr_pair="chr6_chr17"
+chunk="${SLURM_ARRAY_TASK_ID}"
+
+#path to scratch dir
+scratch_root="/home/mcn26/scratch_pi_skr2/mcn26"
+#path where all the model artifact tgzs are
+model_root="${scratch_root}/sumner_pulldown/boda_ensembl_models/${chr_pair}"
+#get all model artifact tgzs
+models=$(find $model_root -maxdepth 1 -type f -printf "%f\n" | sed "s|^|$model_root/|" | paste -sd " " -)
+
+output_dir="${scratch_root}/output/${chr_pair}"
+mkdir -p $output_dir
+
+#make a working directory for just this instance to play in...
+mkdir -p "./${chr_pair}/chunk_${chunk}"
+cd "./${chr_pair}/chunk_${chunk}"
+
+## define & echo the parameters for debugging
+
+vcf_file="${scratch_root}/sumner_pulldown/${chr_pair}/GRCh38-dELS-${chr_pair}chunk_${chunk}.tsv"
+echo "[*] VCF input file ${vcf_file}"
+
+fasta_file="${scratch_root}/GRCh38_no_alt_analysis_set_GCA_000001405.15.fasta"
+echo "[*] Fasta file ${fasta_file}"
+
+output="${output_dir}/chunk_${chunk}.vcf"
+echo "[*] Output ${output}"
+
+echo "[+] Running!"
+
+#invoke actual script
+python "${scratch_root}/sumner_pulldown/boda2/src/vcf_predict.py" \
+--artifact_path ${models} \
+--use_vmap TRUE \
+--vcf_file ${vcf_file} \
+--fasta_file ${fasta_file} \
+--output ${output} \
+--relative_start 9 \
+--relative_end 181 \
+--step_size 10 \
+--raw_predictions FALSE \
+--strand_reduction mean \
+--window_reduction mean \
+--feature_ids K562 HepG2 SKNSH
+
+echo "[+] Finished!"
