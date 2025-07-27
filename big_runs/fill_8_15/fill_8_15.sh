8359a18f136d85352f1e582b298395f405290dbb
Mackenzie Noon <mackenzie.noon@gmail.com>
2025-07-27 13:06:44 -0400

    +8 15

---
+++ big_runs/fill_8_15/fill_8_15.sh
@@ -0,0 +1,51 @@
+#!/bin/bash
+#SBATCH --job-name=f8_15
+#SBATCH --partition=gpu_h200
+#SBATCH --gres=gpu:1
+#SBATCH -c 2
+#SBATCH --mem=60G
+#SBATCH -t 2:30:00
+#SBATCH --array=129,2,6,134,11,139,14,143,145,19,149,23,153,27,31,159,34,163,165,39,41,171,173,46,48,178,181,55,58,187,61,189,67,69,75,77,81,87,91,94,99,103,105,109,115,117,122,127
+
+module load miniconda
+conda activate boda_cu128
+
+chr_pair="chr8_chr15"
+
+#path to scratch dir
+scratch_root="/home/mcn26/scratch_pi_skr2/mcn26"
+#path where all the model artifact tgzs are
+model_root="${scratch_root}/sumner_pulldown/boda_ensembl_models/${chr_pair}"
+#get all model artifact tgzs
+models=$(find $model_root -maxdepth 1 -type f -printf "%f\n" | sed "s|^|$model_root/|" | paste -sd " " -)
+output_dir="${scratch_root}/output/${chr_pair}"
+mkdir -p $output_dir
+
+chunk=$SLURM_ARRAY_TASK_ID
+
+echo "[+] Making ./${chr_pair}/chunk_${chunk}"
+mkdir -p "./${chr_pair}/chunk_${chunk}"
+cd "./${chr_pair}/chunk_${chunk}"
+
+vcf_file="${scratch_root}/sumner_pulldown/${chr_pair}/GRCh38-dELS-${chr_pair}chunk_${chunk}.tsv"
+fasta_file="${scratch_root}/GRCh38_no_alt_analysis_set_GCA_000001405.15.fasta"
+output="${output_dir}/chunk_${chunk}.vcf"
+
+
+echo "[*] Running chunk ${chunk} on host: $(hostname) with CUDA_VISIBLE_DEVICES=${CUDA_VISIBLE_DEVICES}"
+python "${scratch_root}/sumner_pulldown/boda2/src/vcf_predict.py" \
+--artifact_path ${models} \
+--use_vmap TRUE \
+--vcf_file ${vcf_file} \
+--fasta_file ${fasta_file} \
+--output ${output} \
+--relative_start 9 \
+--relative_end 181 \
+--step_size 10 \
+--raw_predictions FALSE \
+--strand_reduction mean \
+--window_reduction mean \
+--feature_ids K562 HepG2 SKNSH  > "chunk_${chunk}.out"
+
+
+echo "[+] Finished!"
