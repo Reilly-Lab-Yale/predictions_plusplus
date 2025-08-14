2e25a309fd058bb08de33548f7d4cef2d93b869d
Mackenzie Noon <mackenzie.noon@gmail.com>
2025-08-14 16:39:35 -0400

    updated for clinpred

--- clinical/2_make_predictions.sh
+++ clinical/2_make_predictions.sh
@@ -1,38 +1,38 @@
 #!/bin/bash
-#SBATCH --job-name=f6_17
+#SBATCH --job-name=clinpred
 #SBATCH --partition=gpu_h200
 #SBATCH --gres=gpu:1
 #SBATCH -c 2
 #SBATCH --mem=64G
 #SBATCH -t 1:45:00
-#SBATCH --array=1-22
+#SBATCH --array=0-10
 
 module load miniconda
 conda activate boda_cu128
 
-chr_pair="chr6_chr17"
+# Build array of pairs
+mapfile -t chromosome_pairs < <(
+    paste -d"_" <(seq -f "chr%g" 1 22) <(seq -f "chr%g" 22 -1 1) | head -n 11
+)
 
-#path to scratch dir
 scratch_root="/home/mcn26/scratch_pi_skr2/mcn26"
+input_root="/home/mcn26/project_pi_skr2/mcn26/clinical_by_chromosome"
+output_root="/home/mcn26/project_pi_skr2/mcn26/clinical_predictions"
+
+chr_pair=${chromosome_pairs[${SLURM_ARRAY_TASK_ID}]}
+
 #path where all the model artifact tgzs are
 model_root="${scratch_root}/sumner_pulldown/boda_ensembl_models/${chr_pair}"
 #get all model artifact tgzs
 models=$(find $model_root -maxdepth 1 -type f -printf "%f\n" | sed "s|^|$model_root/|" | paste -sd " " -)
-output_dir="${scratch_root}/output/${chr_pair}"
-mkdir -p $output_dir
 
-chunk=$SLURM_ARRAY_TASK_ID
 
-echo "[+] Making ./${chr_pair}/chunk_${chunk}"
-mkdir -p "./${chr_pair}/chunk_${chunk}"
-cd "./${chr_pair}/chunk_${chunk}"
-
-vcf_file="${scratch_root}/sumner_pulldown/${chr_pair}/GRCh38-dELS-${chr_pair}chunk_${chunk}.tsv"
+vcf_file="${input_root}/${chr_pair}"
 fasta_file="${scratch_root}/GRCh38_no_alt_analysis_set_GCA_000001405.15.fasta"
-output="${output_dir}/chunk_${chunk}.vcf"
+output="${output_root}/${chr_pair}.vcf"
 
 
-echo "[*] Running chunk ${chunk} on host: $(hostname) with CUDA_VISIBLE_DEVICES=${CUDA_VISIBLE_DEVICES}"
+echo "[*] Running pair ${chr_pair} on host: $(hostname) with CUDA_VISIBLE_DEVICES=${CUDA_VISIBLE_DEVICES}"
 python "${scratch_root}/sumner_pulldown/boda2/src/vcf_predict.py" \
 --artifact_path ${models} \
 --use_vmap TRUE \
@@ -45,7 +45,7 @@ python "${scratch_root}/sumner_pulldown/boda2/src/vcf_predict.py" \
 --raw_predictions FALSE \
 --strand_reduction mean \
 --window_reduction mean \
---feature_ids K562 HepG2 SKNSH  > "chunk_${chunk}.out"
+--feature_ids K562 HepG2 SKNSH
 
 
 echo "[+] Finished!"
