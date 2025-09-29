2ecedb3ed0868ef902ad81baf00bbfed88368d44
Mackenzie Noon <mackenziecnoon@gmail.com>
2025-09-29 10:12:17 -0400

    +dummy columns

--- indel_predictions/2_consolidate/consolidate.sh
+++ indel_predictions/2_consolidate/consolidate.sh
@@ -2,11 +2,19 @@
 #SBATCH -p day
 #SBATCH -t 2:00:00
 #SBATCH -c 1
-#SBATCH --mem-per-cpu=1G
+#SBATCH --mem-per-cpu=8G
 
 data_root="/home/mcn26/scratch_pi_skr2/mcn26/output_indel"
 
+#add tab to fix cols
+
 for d in "${data_root}"/*; do
     bn=$(basename "$d")
-    cat "$d"/* > "${data_root}/${bn}.tsv"
+    out="${data_root}/${bn}.vcf"
+    cat dummy_header.txt > $out
+
+    for f in "$d"/*; do
+	    tail -n +2 "$f" | awk -v OFS="\t" '{print $1, $2, $3, $4, $5, ".", ".", $6}' >> "$out"
+    done
 done
+
